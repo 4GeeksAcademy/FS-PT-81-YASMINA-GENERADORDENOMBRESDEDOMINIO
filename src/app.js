d52d168a9ab6cee8e3c1313cfb7985fbd3c001ad
Yabel90 <yasminabelhaj@hotmail.com>
2024-11-05 19:10:39 +0000

    chore generatordominusfinally

--- src/app.js
+++ src/app.js
@@ -5,7 +5,17 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let pronoun = ["the", "our", "your", "their"];
+let adj = ["great", "big", "small", "slow", "fast"];
+let noun = ["jogger", "racoon", "computer", "school", "city"];
+let cat = [".com", ".net", ".es", ".cat", ".fr"];
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let l = 0; l < cat.length; l++) {
+        console.log(pronoun[i] + adj[j] + noun[k] + cat[l]);
+      }
+    }
+  }
+}
